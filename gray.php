6a02bb385ee4ee8acbc1db7409d413a8230421bb
freetom <tomasbortoli@gmail.com>
2020-01-02 17:17:19 +0100

    add link to reported sites to counter
    Signed-off-by: freetom <tomasbortoli@gmail.com>

---
+++ gray.php
@@ -0,0 +1,19 @@
+<?php
+
+include './api/include/sqlConnect.php';
+include './api/include/functions.php';
+
+$res=query($conn, 'SELECT ns FROM '.$reportTable);
+$res->bind_result($x);
+
+echo '
+<html>
+  <body>';
+while($res->fetch())
+  echo $x.'<br/>';
+echo '
+  <body>
+</html>
+';
+
+?>
--- include/counter.php
+++ include/counter.php
@@ -5,7 +5,7 @@ include './api/include/functions.php';
 <a href="black.php" target="_blank"><div class="counter"><b class="stat"><?php
           echo getRecordsCount($conn, $blackListTable);
           ?></b> sites blocked</div></a>
-<div class="counter" style="background: #D8DBE2;"><b class="stat" id="reported"><?php
+<a href="gray.php" target="_blank"><div class="counter" style="background: #D8DBE2;"><b class="stat" id="reported"><?php
           echo getRecordsCount($conn, $reportTable);
-          ?></b> sites Reported</div>
+          ?></b> sites Reported</div></a>
 <br/><br/>
